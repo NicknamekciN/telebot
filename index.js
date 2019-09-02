d9824d36c53cbf680297988904c9ce4fb8e36461
Vlad <kotbormoglot@1432gmail.com>
2019-09-02 21:13:31 +0300

    Start

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '969367811:AAGR4MkgU0Lb2SIHnNgnNCp5pGzRFnUm9hQ';
 var bot = new TelegramBot(token, {polling: true});
 
-var notes = [];
-
-bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
-    var userId = msg.from.id;
-    var text = match[1];
-    var time = match[2];
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, 'Ill have the tuna. No crust.');
 });
-
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++) {
-        const curDate = new Date().getHours() + ':' + new Date().getMinutes();
-        if (notes[i]['time'] === curDate) {
-            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-            notes.splice(i, 1);
-        }
-    }
-}, 1000);
